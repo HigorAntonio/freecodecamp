4b81470a5f13539c4fd03a25bf4fdfb0766e5eaf
Higor Antonio <higorantonio.ha@gmail.com>
2021-07-22 21:50:53 -0300

    change route '/'

--- apis-and-microservices/00-how-to-use-package-json-the-core-of-any-node-js-project-or-npm-package/boilerplate-npm/server.js
+++ apis-and-microservices/00-how-to-use-package-json-the-core-of-any-node-js-project-or-npm-package/boilerplate-npm/server.js
@@ -38,19 +38,21 @@ app.route('/')
     .get(function(req, res) {
 		  // res.sendFile(process.cwd() + '/views/index.html');
       //joining path of directory 
-const directoryPath = path.join(__dirname);
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file); 
-    });
-});
+      const directoryPath = path.join(__dirname);
+      const files = [];
+      //passsing directoryPath and callback function
+      fs.readdir(directoryPath, function (err, files) {
+          //handling error
+          if (err) {
+              return console.log('Unable to scan directory: ' + err);
+          } 
+          //listing all files using forEach
+          files.forEach(function (file) {
+              // Do whatever you want to do with the file
+              files.push(file);
+          });
+          res.json(files);
+      });
     })
 
 // Respond not found to all the wrong routes
